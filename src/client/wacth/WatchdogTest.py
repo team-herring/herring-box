abaa33e5a90a6713efb325604926d590af869372
youngdeok.k <youngdeok.k@gmail.com>
2013-08-23 00:34:45 -0400

    init

---
+++ src/client/wacth/WatchdogTest.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path="/home/yd/", recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
